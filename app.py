8cf3a98a26a8d44c2ece10d69de499a8aaf2af97
essanest <ehsan.assadiyan@gmail.com>
2025-07-20 22:35:39 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-from flask import Flask, request
-import requests
+from flask import Flask
 
 app = Flask(__name__)
 
-TOKEN = "7988601127:AAFXcCLC94rdXTMNQ_YudTL0VqPBjuhdQ1w"
-URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-@app.route('/', methods=['POST'])
-def webhook():
-    data = request.get_json()
-
-    if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
-        text = data["message"].get("text", "")
-
-        if text.lower() in ["سلام", "salam"]:
-            requests.post(URL, json={
-                "chat_id": chat_id,
-                "text": "سلام! حالت چطوره؟ 😊"
-            })
-
-    return {"ok": True}
+@app.route("/")
+def home():
+    return "🟢 Bot is running on Render!"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
